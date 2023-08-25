6bc225418bfa9ecfb414c4afe4548424f037e3f2
aybjax <aybat93@gmail.com>
2023-08-25 19:31:46 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Plugin for insomnia web client
+## Allows use of NATS request reply
+
+### Use: add url with any HTTP method
+---
+ - #### nats/queuename
+        -- default is: localhost:4222
+ - #### nats:port/queuename
+    -- default is: localhost
+ - #### nats:url:port/queuename
+
+### Use add JSON payload
+
+### Add this plugin
+ - Application => Preferences => Plugins => install
---
+++ main.js
@@ -0,0 +1,76 @@
+// For help writing plugins, visit the documentation to get started:
+//   https://docs.insomnia.rest/insomnia/introduction-to-plugins
+
+// TODO: Add plugin code here...
+
+const bufferToJsonObj = buf => JSON.parse(buf.toString('utf-8'));
+const jsonObjToBuffer = obj => Buffer.from(JSON.stringify(obj), 'utf-8');
+const { connect, Empty, StringCodec } = require("nats");
+
+module.exports.requestHooks = [
+    async function (context) {
+        const route = /^(https?:\/\/)?nats(:[a-zA-z0-9-_]+)?(:\d+)\/(.+)$/.exec(context.request.getUrl());
+
+        if(!route) {
+            return
+        }
+        
+        console.log('route is nats')
+
+        const url = route[2] ?? 'localhost'
+        const port = route[3] ?? ':4222'
+        const queue = route[4];
+        context.request.addHeader('url', url)
+        context.request.addHeader('port', port)
+        context.request.addHeader('queue', queue)
+        context.request.addHeader('body', JSON.stringify(context.request.getBody().text))
+
+        context.request.setBody({})
+        context.request.setMethod('GET')
+        context.request.setUrl(`https://google.com`)
+    }
+]
+
+module.exports.responseHooks = [
+    async function (context) {
+        if(context.request.getUrl() !== 'https://google.com') {
+            console.log('route is not nats')
+
+            return
+        }
+
+        const url = context.request.getHeader('url')
+        const port = context.request.getHeader('port')
+        const queue = context.request.getHeader('queue')
+        const body = context.request.getHeader('body')
+
+        console.log({
+            url, port, queue, body
+        })
+
+        const nc = await connect({ servers: `${url}${port}` });
+
+        // create an encoder
+        const sc = StringCodec();
+        let response = {};
+
+        // the client makes a request and receives a promise for a message
+        // by default the request times out after 1s (1000 millis) and has
+        // no payload.
+        await nc.request(queue, JSON.parse(body), { timeout: 1000 })
+        .then((m) => {
+            console.log(`got response: ${sc.decode(m.data)}`);
+            context.response.setBody(sc.decode(m.data))
+        })
+        .catch((err) => {
+            console.log(`problem with request: ${err.message}`);
+            context.response.setBody(JSON.stringify({
+                error: err
+            }))
+        });
+
+        await nc.close();
+
+        console.log('request hook end')
+    }
+]
